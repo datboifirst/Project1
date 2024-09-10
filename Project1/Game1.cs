1c7afaeb339541cb14757ca91f928bb5eb8009ec
datboifirst <khunnapat47@gmail.com>
2024-09-10 14:32:58 +0700

    update feature1

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -8,7 +8,7 @@ namespace Project1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        //
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
